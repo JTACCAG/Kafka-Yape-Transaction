822d50f8b7365d67df4802ad5c9df2e7fedd3e03
JTACCAG <jtaccag@gmail.com>
2022-11-17 16:57:30 -0500

    add microservice kafka

--- services/anti-fraud/src/main.ts
+++ services/anti-fraud/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { v4 as uuid } from 'uuid';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          // brokers: ['kafka:29092'],
+          brokers: ['localhost:9092'],
+        },
+        consumer: {
+          groupId: `user.${uuid()}`,
+        },
+      },
+    },
+  );
+
+  await app
+    .listen()
+    .then(() => Logger.log('Microservice is listening'))
+    .catch(() => Logger.log('Microservice is not listening'));
 }
 bootstrap();
